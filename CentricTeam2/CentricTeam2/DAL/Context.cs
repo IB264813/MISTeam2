c5d9910ee52f39ed5cfd1ae871fc425e12b63980
Ian Beith <ib264813@ohio.edu>
2017-10-22 18:29:18 -0400

    Added DAL and UserDetail model

---
+++ CentricTeam2/CentricTeam2/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CentricTeam2.Models;
+using System.Data.Entity;
+
+namespace CentricTeam2.DAL
+{
+    public class Context : DbContext
+    {
+
+
+
+    }
+}
