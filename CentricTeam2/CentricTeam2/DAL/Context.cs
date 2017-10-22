089e36640ffcb53ab22411d60acac73c479ca8d0
Ian Beith <ib264813@ohio.edu>
2017-10-22 19:25:50 -0400

    Changes Made

--- CentricTeam2/CentricTeam2/DAL/Context.cs
+++ CentricTeam2/CentricTeam2/DAL/Context.cs
@@ -9,8 +9,18 @@ namespace CentricTeam2.DAL
 {
     public class Context : DbContext
     {
+        public Context(): base("name=cs4200")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DAL.Context, CentricTeam2.Migrations.Context.Configuration>("cs4200"));
 
+        }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
 
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public System.Data.Entity.DbSet<CentricTeam2.Models.UserDetails> UserDetails { get; set; }
     }
 }
