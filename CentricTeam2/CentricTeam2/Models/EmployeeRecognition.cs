2ec66febe82e99e5d1f58898d8a43b3493423b96
bs772812 <bs772812@COB-LABBT>
2017-12-04 10:36:47 -0500

    Employee Recognition

---
+++ CentricTeam2/CentricTeam2/Models/EmployeeRecognition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CentricTeam2.Models
+{
+    public class EmployeeRecognition
+    {
+        public Guid EmployeeRecognitionID { get; set; }
+
+        [Display(Name = "Employee Getting Recognition")]
+        [Required(ErrorMessage = "Please select an employee")]
+
+        public Guid employeeId { get; set; }
+        [ForeignKey("Employeeid")]
+
+        public virtual UserDetails Employee { get; set; }
+        [Display(Name = "Employee Giving Recognition")]
+
+        public Guid recognizerId { get; set; }
+        [ForeignKey("Recognizerid")]
+
+        public virtual UserDetails Recognizer { get; set; }
+        [Display(Name = "Recogniton Awarded")]
+
+        public Guid recognitionId { get; set; }
+        [ForeignKey("recognitionId")]
+
+        public virtual Recognition Recognition { get; set; }
+        [Display(Name = "Date Awarded")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode =true)]
+
+        public Nullable<System.DateTime> awardDate { get; set; }
+        [Display(Name = "Why is this person getting this award")]
+        [Required(ErrorMessage = "Please enter why this person is getting this award")]
+
+        public string description { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
